73ba5d3bd5269d7e6881ec79b6fc0121ff2fb03e
MoriokaReimen <nakata.kei.job@gmail.com>
2021-02-07 20:55:33 +0900

    [update] complete system

--- Model/HeaderGenerator.py
+++ Model/HeaderGenerator.py
@@ -2,6 +2,7 @@ import Model.DataClass
 from typing import List
 import re
 import datetime
+import os
 
 def generateHeader(header_config: Model.DataClass.HeaderConfig, configs: List[Model.DataClass.ConfigData], val_configs: List[Model.DataClass.ValConfigData]) -> str:
     contents: str = str()
@@ -9,8 +10,11 @@ def generateHeader(header_config: Model.DataClass.HeaderConfig, configs: List[Mo
     header = header_config.header
     date_time = datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
     header = re.sub("\$\{DATE_TIME\}", date_time, header)
+
+    file_name = os.path.basename(header_config.path)
+    header = re.sub("\$\{FILE_NAME\}", file_name, header)
+
     contents += header
-    contents += "\r\n"
     contents += "\r\n"
     contents += "/* Enable/Disable Configurations ******************************************/\r\n"
     for config in configs:
@@ -18,9 +22,9 @@ def generateHeader(header_config: Model.DataClass.HeaderConfig, configs: List[Mo
             continue
         contents += "#define\t"
         contents += config.symbol
-        contents += "\t/* "
+        contents += "\t/**< "
         contents += config.description
-        contents += "*/\r\n"
+        contents += " */\r\n"
     contents += "\r\n"
 
     contents += "/* Value Configurations ***************************************************/\r\n"
@@ -32,13 +36,18 @@ def generateHeader(header_config: Model.DataClass.HeaderConfig, configs: List[Mo
         contents += "\t( "
         contents += str(val_config.value)
         contents += " )"
-        contents += "\t/* "
+        contents += "\t/**< "
         contents += config.description
-        contents += "*/\r\n"
+        contents += " */\r\n"
     
     contents += "\r\n"
     footer = header_config.footer
+
     date_time = datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
     footer = re.sub("\$\{DATE_TIME\}", date_time, footer)
+
+    file_name = os.path.basename(header_config.path)
+    footer = re.sub("\$\{FILE_NAME\}", file_name, footer)
+
     contents += footer
     return contents
